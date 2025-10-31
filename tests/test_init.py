cd59ac26beb586b82cf94ded5854636abe3ad4e5
FerdiHS <ferdi.fhs.fhs@gmail.com>
2025-11-01 01:48:24 +0800

    Add precommit hooks

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,3 +1,12 @@
+"""
+Unit test for importing the dctp module and checking its version attribute.
+"""
+
+
 def test_import_and_version():
-    import ahtp
-    assert isinstance(ahtp.__version__, str) and len(ahtp.__version__) > 0
+    """
+    Test that the dctp module can be imported and has a valid __version__ attribute.
+    """
+    import dctp
+
+    assert isinstance(dctp.__version__, str) and len(dctp.__version__) > 0
